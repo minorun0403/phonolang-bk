2c6a4d31fc9fb18185054a2ebb713e3c34e5895d
sttut <als4626dkb@gmail.com>
2025-04-02 03:06:47 +0900

    add lesson page and answer page
    - Completed process from lesson page to answer page (hard-coded)
    - not used with DB

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LessonController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {return view('index');});
+
+Route::get('/lesson', [LessonController::class, 'entrypoint'])->name('lesson.entrypoint');
+Route::post('/lesson/{lesson_id}/answer/{question_no}', [LessonController::class, 'answer'])->name('lesson.answer');
